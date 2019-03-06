1e1ffc7978bc12914dbb68e0bbab8b2297cba928
Xaver <xaverfischer@mail.de>
2019-03-06 23:04:23 +0100

    added quassel

--- create_links.sh
+++ create_links.sh
@@ -3,4 +3,5 @@ repo_path=$(dirname $(readlink -f $0))
 ln -s "$repo_path/.vimrc" ~/.vimrc
 ln -s "$repo_path/.tmux.conf" ~/.tmux.conf
 ln -s "$repo_path/.bash_aliases" ~/.bash_aliases
+ln -s "$repo_path/.quassel" ~/.quassel
 
