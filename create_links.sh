ef521a500b3c33363430dc3db4528c41b7475560
Xaver <xaverfischer@mail.de>
2019-07-07 12:46:53 +0200

    added i3wm config, refactoring

--- create_links.sh
+++ create_links.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
 repo_path=$(dirname $(readlink -f $0))
 
+function create_link {
+    echo "$1 -> $2"
+    ln -sfn $1 $2
+}
 
 find $repo_path -name '.*' | grep -v '.git' | while read file
 do
-    echo "$file"
-    ln -sfn "$file" ~/$(basename $file)
+    create_link "$file" ~/$(basename $file)
 done
+
+# i3wm config
+create_link "$repo_path/config" ~/.config/i3/config
+
